98b5cf402e78a62fa39a088b7a202914a3503e0e
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-09 21:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    //Add your code here
+    const prices = document.querySelectorAll(".price");
+    let totalPrice = 0;
+    prices.forEach(price => {
+        totalPrice += Number(price.innerText.replace("$", ""));
+    });
+    // alert(`Total price: $${totalPrice.toFixed(2)}`);
+    // console.log(totalPrice);
+    const totalRow = document.createElement("tr");
+    totalRow.innerHTML = `
+            <td>Total</td>
+            <td id="ans">${totalPrice}</td>
+    `;
+    document.querySelector("table").appendChild(totalRow);
+
 };
 
 getSumBtn.addEventListener("click", getSum);
-
